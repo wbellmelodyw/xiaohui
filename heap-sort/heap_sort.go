7f91f9206f60bdf798bbfd4a0d92fec56ad48707
wbellmelodyw <senkai0903@vip.qq.com>
2020-04-27 11:12:26 +0800

    堆排序

--- heap-sort/heap_sort.go
+++ heap-sort/heap_sort.go
@@ -1,6 +1,38 @@
 package heap_sort
 
-//堆排序
-func downAdJust() {
+import "fmt"
 
+//最大堆下沉
+func downAdJust(a *[]int, parentIndex int, length int) {
+	array := *a
+	temp := array[parentIndex]
+	childrenIndex := parentIndex*2 + 1
+	for childrenIndex < length {
+		//取最大的子节点
+		if childrenIndex+1 < length && array[childrenIndex] < array[childrenIndex+1] {
+			childrenIndex++
+		}
+		if temp >= array[childrenIndex] {
+			break
+		}
+		array[parentIndex] = array[childrenIndex]
+		parentIndex = childrenIndex
+		childrenIndex = childrenIndex*2 + 1
+	}
+	array[parentIndex] = temp
+}
+
+func heapSort(a *[]int) {
+	array := *a
+	length := len(array)
+	//先把无序数组构建成最大堆
+	for i := (length - 2) / 2; i >= 0; i-- {
+		downAdJust(a, i, length)
+	}
+	fmt.Println(array)
+	//头和尾互换
+	for i := length - 1; i > 0; i-- {
+		array[0], array[i] = array[i], array[0]
+		downAdJust(a, 0, i) //避免最大被重新弄上去
+	}
 }
--- heap-sort/heap_sort_test.go
+++ heap-sort/heap_sort_test.go
@@ -1,1 +1,12 @@
 package heap_sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHeapSort(t *testing.T) {
+	a := &[]int{1, 3, 2, 6, 5, 7, 8, 9, 10, 0}
+	heapSort(a)
+	fmt.Println(a)
+}
